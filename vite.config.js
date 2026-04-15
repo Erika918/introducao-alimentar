72588043bd04ab5ff06ec76d683060da642e1774
Erika Barbosa <erikaregis42@gmail.com>
2026-04-15 11:24:45 -0300

    config: adiciona vercel.json e ajustes do projeto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: './', // MUDE PARA './' (com um ponto antes da barra)
 })
